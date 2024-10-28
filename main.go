b73033ac489db6f532eb64139ae38405e581218f
Samyog Ghimire <samyog.ghimire25@gmail.com>
2024-10-28 14:30:56 +0530

    More codes for the booking app

--- main.go
+++ main.go
@@ -8,12 +8,9 @@ import (
 func main() {
 	var conferenceName string = "Go Conference"
 	const conferenceTickets = 50
-
 	var remainingTickets = 50
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets here to attend\n")
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for remainingTickets > 0 {
 
@@ -35,11 +32,10 @@ func main() {
 
 		remainingTickets = remainingTickets - userTickets
 
-		isValidEmail := strings.Contains(email, "@") //strings package has Contains which checks whether the given value is present or not
-		isValidTicketNumber := userTickets < 0 && userTickets <= remainingTickets
+		validation(email, userTickets, remainingTickets)
 
 		//if user wants to book more tickets than remaining tickets
-		if isValidEmail && isValidTicketNumber {
+		if isValidEmail && !isValidTicketNumber {
 
 			var bookings []string
 			bookings = append(bookings, firstName+" "+lastName)
@@ -47,16 +43,8 @@ func main() {
 			fmt.Printf("Thank You %v %v for booking %v tickets. You will receive the email of your tickets at %v\n", firstName, lastName, userTickets, email)
 			fmt.Printf("%v tickets remain for the conference %v.\n", remainingTickets, conferenceName)
 
-			firstNames := []string{}
-			//this lines below defines whole logic of getting first name of the user
-			for _, booking := range bookings { //_ is the blank identifier (ignores the unused variable ) used when you want
-				var names = strings.Fields(booking)       //splits the booking string into words based on whitespace
-				firstNames = append(firstNames, names[0]) //append the first name to the list
-				//}
-
-				// fmt.Printf("First names:%v\n", firstNames) //print the first names
-				fmt.Printf("These are all our bookings: %v\n", firstNames) //only provides first name of the people who bought tickets
-			}
+			firstNames := fname(bookings)
+			fmt.Printf("These are all our bookings: %v\n", firstNames) //only provides first name of the people who bought tickets
 
 			if remainingTickets == 0 {
 				fmt.Println("Our conference is fully booked. Come back next year.")
@@ -79,4 +67,31 @@ func main() {
 		}
 
 	}
+
+}
+
+func greetUsers(conferenceName string, conferenceTickets int, remainingTickets int) {
+	// var conference = conferenceName
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend the conference\n")
+
+}
+
+func fname(bookings []string) []string { //here the seond []string denotes the firstname is also a slice of string
+	firstNames := []string{}
+	//this lines below defines whole logic of getting first name of the user
+	for _, booking := range bookings { //_ is the blank identifier (ignores the unused variable ) used when you want
+		var names = strings.Fields(booking)       //splits the booking string into words based on whitespace
+		firstNames = append(firstNames, names[0]) //append the first name to the list
+
+		return firstNames
+
+	}
+}
+
+func validation(email string, userTickets int, remainingTickets int) (bool, bool) {
+	isValidEmail := strings.Contains(email, "@") //strings package has Contains which checks whether the given value is present or not
+	isValidTicketNumber := userTickets < 0 && userTickets <= remainingTickets
+	return isValidEmail, isValidTicketNumber
 }
