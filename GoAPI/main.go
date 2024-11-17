b9f2d6b561a9446f1a7b2558a5f49bfb5957a57c
Samyog Ghimire <samyog.ghimire25@gmail.com>
2024-11-17 23:24:02 +0530

    Adding code to add and return a book

--- GoAPI/main.go
+++ GoAPI/main.go
@@ -20,6 +20,7 @@ var books = []book{
 	{ID: "3", Title: "Rich Dad Poor Dad", Author: "Someone", Quantity: 5},
 }
 
+// Code to get all the books
 func getBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
@@ -34,6 +35,8 @@ func bookID(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusOK, book)
 }
+
+// Code to get a book by its id
 func getBookByID(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
@@ -43,6 +46,53 @@ func getBookByID(id string) (*book, error) {
 	return nil, errors.New("Book not found")
 }
 
+// Code or checking out a book
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id parameter"})
+		return
+	}
+	book, err := getBookByID(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 1 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+// code to return the book
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id parameter"})
+		return
+	}
+	book, err := getBookByID(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 1 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+// Code to add a new book to the list
 func createBook(c *gin.Context) {
 	var newBook book
 
@@ -55,11 +105,17 @@ func createBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newBook)
 
 }
+
+// Router
 func main() {
 	router := gin.Default()
-	router.SetTrustedProxies([]string{"127.0.0.1"})
+	// router.SetTrustedProxies([]string{"127.0.0.1"})
 	router.GET("/books", getBook)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+
+	router.GET("/books/:id", bookID)
+	router.PATCH("/return", returnBook)
+
 	router.Run("localhost:8081")
-	router.GET("/books/:id")
 }
