4bb8512a481bc6633d19276d74c528c16866bf0d
Samyog-G <samyogghimire25@gmail.com>
2025-01-04 16:37:22 +0530

    Golang jwt authetication

---
+++ models/userModel.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID           primitive.ObjectID `bsin:"id"`
+	FirstName    *string            `json:"firstname" validate:"required, min=2, max=100"`
+	LastName     *string            `json:"lastname"`
+	Password     *string            `json:"password" validate:required, min=8`
+	Email        *string            `json:"email"`
+	Phone        *string            `json:"phone"`
+	Token        *string            `json:"token"`
+	UserType     *string            `json:"usertype"`
+	RefreshToken *string            `json:"refreshtoken"`
+	CreatedAt    *time.Time         `json:"createdat"`
+	UpdatedAt    *time.Time         `json:"updatedat"`
+	UserID       *string
+}
