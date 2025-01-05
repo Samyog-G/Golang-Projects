2d696a6b4c59599674a155e7d1091e7d92c3e477
Samyog Ghimire <samyog.ghimire25@gmail.com>
2025-01-05 22:58:12 +0530

    "Learning JWT Authentication Using golang"

---
+++ jwt-authentication/helpers/authHelper.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserTyper(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("Unauthorized access")
+		return err
+	}
+	return err
+}
+func MatchUserTypeToUid(c *gin.Context, userID string) (err error) {
+	userType := c.GetString("user type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userID {
+		err = errors.New("Unauthorized access")
+		return err
+	}
+	err = CheckUserTyper(c, userType)
+	return err
+}
