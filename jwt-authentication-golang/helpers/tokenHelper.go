95153822ab871e1ed086e0135e28e026734387ff
Samyog Ghimire <samyog.ghimire25@gmail.com>
2025-01-15 14:00:27 +0530

    Update tokenHelper.go

--- jwt-authentication-golang/helpers/tokenHelper.go
+++ jwt-authentication-golang/helpers/tokenHelper.go
@@ -98,6 +98,28 @@ func UpdateAllTokens(userID string, signedToken string, signedRefreshToken strin
 	}
 }
 
-func ValidateToken() {
-
+func ValidateToken(signedToken string) (claims *SignedDetails,msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("The token is invalid")
+		msg = err.Error()
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("Token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
 }
