9e60b2e6805b77c28857a677e29406bd7558435e
Samyog Ghimire <samyog.ghimire25@gmail.com>
2025-01-15 13:58:42 +0530

    Update userModel.go

--- jwt-authentication-golang/models/userModel.go
+++ jwt-authentication-golang/models/userModel.go
@@ -10,7 +10,7 @@ type User struct {
 	ID           primitive.ObjectID `bsin:"id"`
 	FirstName    *string            `json:"firstname" validate:"required, min=2, max=100"`
 	LastName     *string            `json:"lastname"`
-	Password     *string            `json:"password" validate:required, min=8`
+	Password     *string            `json:"password" validate:"required, min=8"`
 	Email        *string            `json:"email"`
 	Phone        *string            `json:"phone"`
 	Token        *string            `json:"token"`
